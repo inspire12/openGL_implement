ac3cbf87af1fdf8a7492828762e301fead02b19f
seo,yeonghak <ox4443@naver.com>
2016-12-18 21:02:16 +0900

    Implement Command and Circle moving
    a-s-d-w and up-down-left-down key map the spin moving.
    plus texture mapping

--- openGL_SpinTop/SpinTop/소스.cpp
+++ openGL_SpinTop/SpinTop/소스.cpp
@@ -11,37 +11,44 @@ typedef struct color {
 	double b;
 }color;
 static double Power[2] = { 1000, 1000 }; 
-static double moveAX = 0.4, moveAH = 0.4, rotateA = 1, accA = 1;
+static double moveAX = 0.4, moveAH = 0.4, rotateA = 1, accA = -1, mA = 10;
 double size = 1;
-static double moveBX = -0.4, moveBH = 0.4, rotateB = 1, accB = 1;
+static double moveBX = -0.4, moveBH = 0.4, rotateB = 1, accB = 1, mB = 10;
 
+color c[2] = { {3,0,1},{0,3,1} };
 void initLight();
 void reshape(int w, int h);
 
 // 시간 가는 거 즉, 회전하는 것과 힘을 조금씩 줄이는 것
 void timeGoesOn() {
-	moveAX = (moveAX - 1);
+	moveAX = (moveAX - accA);
 	if (moveAX > 360) moveAX -= 360;
-	moveBX = (moveBX - 1);
+	else if(moveAX <-360)  moveAX += 360;
+	
+	moveBX = (moveBX - accB);     
 	if (moveBX > 360) moveBX -= 360;
+	else if(moveBX <-360)  moveBX += 360;
+	
 	Sleep(20);
 	glutPostRedisplay();
 }
 
-//팽이 만드는 것, 위치와 크기, 회전까지 
+float x = 10, y = 10, z= 10;
 
 void makeArena() {
 	// 지형을 그립니다.
 	glColor3f(0.9f, 0.9f, 0.9f);
-	glBegin(GL_QUAD_STRIP);
-		glVertex3f(-100.0f, 0.0f, -100.0f);
-		glVertex3f(-100.0f, 0.0f, 100.0f);
-
-		glVertex3f(100.0f, 0.0f, 100.0f);
-		glVertex3f(100.0f, 0.0f, -100.0f);
+	glBegin(GL_QUADS);
+		glVertex3f(x, y, z);
+		glVertex3f(x+10, y+10, z);
+		glVertex3f(x + 10, y, z + 10);
+		glVertex3f(x, y + 10, z + 10);
 	glEnd();
 }
-void makeSpinTop(double move, double moveZ, int rv) {
+
+
+//팽이 만드는 것, 위치와 크기, 회전까지 
+void makeSpinTop(double move, double moveZ, int rv, color c) {
 	glLoadIdentity();
 	gluLookAt(0.3, 0.15, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0);
 	//	gluLookAt(0.1, 0., 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0);
@@ -51,39 +58,87 @@ void makeSpinTop(double move, double moveZ, int rv) {
 	double distance = size - moveZ;
 
 	glRotatef((GLfloat)move, 0.0, 0.0, 1.0);//좌우 움직임
-	glTranslatef(sqrt(distance), sqrt(distance), 0.0);	//distance 값 계산 필요 경기장 위치의 반지름
+	glTranslatef(distance, 0.0, distance);	//distance 값 계산 필요 경기장 위치의 반지름
 
-	glRotatef((GLfloat)rv, 0.0, 1.0, 0.0); //회전 
-	glColor3f(3, 0, 1);
+	glRotatef((GLfloat)rv, 0.0, 0.0, 1.0); //회전 
+	glColor3f(c.r, c.g, c.b);
 	glutSolidCone(0.03, 0.05, 80, 80); //원뿔 출력
 }
-
+double abs(double a) {
+	if (a < 0)return -a;
+	else return a;
+}
+double rangeX = 25, rangeY = 0.2;
+bool isCollapse() {
+	if(moveAH  <= moveBH + rangeY && moveAH  >= moveBH - rangeY){
+		if (abs(moveAX) <= abs(moveBX) + rangeX && abs(moveAX)  >= abs(moveBH) - rangeX) {
+			return true;
+		}
+	}
+	return false;
+}
 void MyDisplay() {
 	initLight();
-
-	gluLookAt(0.3, 0.15, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0);
-//	makeArena();
+	gluLookAt(0.3, 0.15, 0.3, 0.0, 0.0, -10.0, 0.0, 0.0, -1.0);
+	makeArena();
 	//glutSolidSphere((GLdouble)0.3, 2000, 1600); // 태양을 그려냄
 	//경기장 그리기!
 	glPushMatrix(); 
-	//for (int i = 0; i<8; i++) {
 	
-		makeSpinTop(moveAX, moveAH, rotateA);
-		makeSpinTop(moveBX, moveBH, rotateB);
-	//}
+	//여기선 힘과 기존 속도에 따라 move를 정해준다. 
+
+
+	//여기선 충돌처리
+	if (isCollapse()){  // 콘 0.02
+		//방향이 반대일 때 속도를 바꿔주고 거리를 벌려주자 
+		if(accA * accB <0){
+			double tmp = accA;
+			accA = accB;
+			accB = tmp;
+			if (accA < 0) {
+				moveAX += rangeX * 10 / 2;
+				moveBX -= rangeX * 10 / 2;
+			}
+			else {
+				moveAX -= rangeX * 10 / 2;
+				moveBX += rangeX * 10 / 2;
+			}
+		}
+		//방향이 같을 때 
+		else {
+			if (moveAX > moveBX) {
+				accA += 30;
+				accB -= 30;
+			}
+			else {
+				accA -= 30;
+				accB += 30;
+			}
+		}
+	}
 	timeGoesOn();
+
+	makeSpinTop(moveAX, moveAH, rotateA, c[0]);
+	makeSpinTop(moveBX, moveBH, rotateB, c[1]);
+	
+		
+	
 	glPopMatrix();
 	glutSwapBuffers();
 }
 
-bool buffer[256];
 
+// 키입력 속도 아님
 void MyKeyboard(unsigned char key, int x, int y) {
-	buffer[key] = true;
-
+	fflush(stdin);
 	switch (key) {
 	case 'd':
-		moveBX = (moveBX + 5);
+		if (accB > 0) accB = -1;
+		else {
+			accB -= 0.5;
+			if (accB < -3) accB += 0.5;
+		}
+		
 		if (moveBX > 360) moveBX -= 360;
 		glutPostRedisplay();
 		break;
@@ -93,7 +148,12 @@ void MyKeyboard(unsigned char key, int x, int y) {
 		glutPostRedisplay();
 		break;
 	case 'a':
-		moveBX = (moveBX - 5);
+		if (accB < 0) accB = 1;
+		else {
+			accB += 0.5;
+			if (accB > 3) accB -= 0.5;
+		}
+		accB = 2;
 		if (moveBX > 360) moveBX -= 360;
 		glutPostRedisplay();
 		break;
@@ -101,6 +161,9 @@ void MyKeyboard(unsigned char key, int x, int y) {
 		moveBH = (moveBH - 0.01);
 		if (moveBH<0) moveBH = (moveBH + 0.01);
 		glutPostRedisplay();
+		break;
+// test 
+	case 'f':
 		break;
 	default:
 		break;
@@ -113,12 +176,12 @@ void inputKey(int key, int x, int y)
 	switch (key)
 	{
 	case GLUT_KEY_LEFT:
-		moveAX = (moveAX - 5);
+		accA = -2;
 		if (moveAX > 360) moveAX -= 360;
 		glutPostRedisplay();
 		break;
 	case GLUT_KEY_RIGHT:
-		moveAX = (moveAX + 5);
+		accA = 2;
 		if (moveAX > 360) moveAX -= 360;
 		glutPostRedisplay();
 		break;
@@ -173,7 +236,7 @@ void initLight() {
 
 void reshape(int w, int h) {
 	float ratio = 1;
-	glViewport(0, 100, w, h);
+	glViewport(100, 200, w, h);
 	gluPerspective(45, ratio, 1, 1000);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
